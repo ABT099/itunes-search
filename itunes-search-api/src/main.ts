07fcb0efd70fe0c7a73bfab8937561f485887645
abt099 <abt099>
2025-06-04 09:11:49 +0300

    disabled cors

--- itunes-search-api/src/main.ts
+++ itunes-search-api/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['https://itunes-search-six.vercel.app'] });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
