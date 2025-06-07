3ea8403c729a28b25af5997bd7c5cb16331918c6
ABT099 <abdo.formal99@gmail.com>
2025-06-07 23:18:54 +0300

    mobile ui fixes

--- itunes-search-ui/src/app/page.tsx
+++ itunes-search-ui/src/app/page.tsx
@@ -293,7 +293,7 @@ export default function Home() {
       </div>
 
       {/* Content */}
-      <div className="max-w-7xl mx-auto py-4 md:py-5 lg:py-6">
+      <div className="max-w-7xl mx-auto px-4 md:px-0 py-4 md:py-5 lg:py-6">
         <FilterBar
           filters={filters}
           onFilterChange={setFilters}
